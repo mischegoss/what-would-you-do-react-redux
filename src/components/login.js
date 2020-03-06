a679cd10afeaa9a7b203226501b65a523cb73f61
Tamar <mischegoss@gmail.com>
2020-03-06 15:12:04 -0500

    Add Login base component

---
+++ src/components/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import { connect } from 'react-redux'
+//import { Redirect, withRouter } from 'react-router-dom'
+//import { setAuthUser, removeAuthUser } from '../actions/authedUser';
+
+class Login extends Component {
+
+    render() {
+        return (
+            <p>Stuff</p>
+
+        )
+    }
+
+}
+
+export default Login;
