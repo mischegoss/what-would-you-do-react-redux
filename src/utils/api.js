997df86dd29f4b7c241b7db1e1de7837a92888f1
Tamar <mischegoss@gmail.com>
2020-03-06 11:25:38 -0500

    Initial set-up following chirper

---
+++ src/utils/api.js
@@ -0,0 +1,30 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer,
+    formatQuestion
+  } from './_DATA.js'
+  
+  export function getInitialData () {
+    return Promise.all([
+      _getUsers(),
+      _getQuestions(),
+    ]).then(([users, questions]) => ({
+      users,
+      questions,
+    }))
+  }
+  
+  export function saveQuestion (info) {
+    console.log('saving', info)
+    return _saveQuestion(info)
+  }
+  
+  export function saveQuestionAnswer (info) {
+    return _saveQuestionAnswer(info)
+  }
+
+  export function formatNewQuestion (info) {
+    return formatQuestion(info)
+  }
