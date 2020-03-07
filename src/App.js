755380a8dd6ce995b27b9dbcfb64041284fbde01
Tamar <mischegoss@gmail.com>
2020-03-07 14:33:29 -0500

    Chore: Remove more default text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import Login from './components/login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+      
         <Login/>
     </div>
   );
